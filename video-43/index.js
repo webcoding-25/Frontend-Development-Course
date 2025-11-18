1f6ee4626dac6aeb2d52369827cdbe2ebc7729f8
WebCoding <167964687+webcoding-25@users.noreply.github.com>
2025-11-18 15:29:56 +0530

    Add files via upload

---
+++ video-43/index.js
@@ -0,0 +1,47 @@
+// console.log("hello students !")
+// console.log("hello students !")
+// console.log("hello students !")
+// console.log("hello students !")
+// console.log("hello students !")
+// console.log("hello students !")
+
+// function syntax
+
+// function hello(){
+// 	console.log("hello students!")
+// }
+// hello();
+// hello();
+// hello();
+// hello();
+// hello();
+// hello();
+// hello();
+
+// function with parameter and argument
+
+// function hello(name){
+// 	console.log("hello " + name)
+// }
+// hello("ram");
+// hello("moHAN");
+// hello("SHYAM");
+
+// function with return keyword
+
+function add(x , y , z=2){
+//    console.log(x+y);
+console.log(x,y,z);
+return x+y+z
+}
+const result1 = add(4)
+const result2 = add(14,5)
+const result3 = add(1,15, 5)
+
+console.log("The sum of this no. is :" , result1);
+console.log("The sum of this no. is :" , result2);
+console.log("The sum of this no. is :" , result3);
+
+
+
+
